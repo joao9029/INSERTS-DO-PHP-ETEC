82e30fa10aadfb882cf31e70f2c70a6a40f05244
joao9029 <joaolucasLicao@gmail.com>
2026-08-27 22:47:15 -0300

    Modify database connection settings and charset
    Updated database connection parameters and set charset to UTF-8.

--- InsertsPHP/inc/conexao.php
+++ InsertsPHP/inc/conexao.php
@@ -1,12 +1,14 @@
 <?php
 $servidor = "localhost";
-$banco = "ETEC";
+$banco = "etec";
 $usuario = "root";
-$senha = "";
+$senha = "usbw";
 
 $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
 if($conexao->connect_error){
-    die("Erro de conexão: " . $conexao->connect_error);
+    die("Erro de conexao: " . $conexao->connect_error);
 }
+
+mysqli_set_charset($conexao, "utf8");
 ?>
